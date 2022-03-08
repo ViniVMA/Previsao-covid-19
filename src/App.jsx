64117df4f25c5b84eb16d84d9d7549f3bf43f2de
ViniVMA <ctt.viniciusvilela@gmail.com>
2022-03-08 16:16:13 -0300

    nessa tentativa eu pensei em obter os dados que eu encontrei no repositorio que foi disponibilizado e obter por ele os dados dos ultimos 7 dias

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [covid7Days, setCovid7Days] = useState(0)
 
+  useEffect(() => {
+    axios.get("https://covid.ourworldindata.org/data/latest/owid-covid-latest.json")
+      .then((response) => {
+        const arr = Object.values(response.data);
+        const map = arr.map((arr) => arr.new_cases_smoothed);
+        const initialValue = 0;
+        const last7Days = map.reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          initialValue,
+        );
+          setCount(last7Days)
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(covid7Days)
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+   <h1>Hello world</h1>
   )
 }
 
