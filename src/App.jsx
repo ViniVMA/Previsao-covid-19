09ad885e2d702ff70c117774b07ff3f717a55791
ViniVMA <ctt.viniciusvilela@gmail.com>
2022-03-08 20:19:18 -0300

    aqui eu percebi que eu poderia fazer de um jeito bem diferente do que eu pensei antes, parei de puxar os dados de todos os paises e resolvi utilizar apenas os do Brasil, preparei a função que faz uma função exponencial pra calcular os dados puxados do json, e o conteudo agora esta dinamico, comecei também a implementar os dados na interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useLayoutEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
 
 function App() {
-  const [covid7Days, setCovid7Days] = useState(0)
+  const [covid7Days, setCovid7Days] = useState()
+  const [totalCases, setTotalCases] = useState(null)
+  const [prediction, setPrediction] = useState([])
+
+  
 
   useEffect(() => {
     axios.get("https://covid.ourworldindata.org/data/latest/owid-covid-latest.json")
       .then((response) => {
-        const arr = Object.values(response.data);
-        const map = arr.map((arr) => arr.new_cases_smoothed);
-        const initialValue = 0;
-        const last7Days = map.reduce(
-          (previousValue, currentValue) => previousValue + currentValue,
-          initialValue,
-        );
-          setCount(last7Days)
+        const data = response.data
+        const dailyCasesData = data.BRA.new_cases
+        const totalCasesData = data.BRA.total_cases
+        setTotalCases(totalCasesData)
+        setCovid7Days(dailyCasesData)
       })
       .catch((error) => console.error(error));
   }, []);
 
   console.log(covid7Days)
+  console.log(totalCases)
 
 
 
   
+  function covidPrediction(n) {
+    var covidNewCases = [],
+      a = covid7Days,
+      b = totalCases;
+  
+    for (var k = 0; k < n; k++) {
+      covidNewCases.push({ dia: 1 * k, casos: b + a * Math.pow(2, 1 * k) });
+    }
+    setPrediction(covidNewCases);
+  }
+  
+  console.log(prediction)
+
+
+  
+
+
   return (
+   <>
    <h1>Hello world</h1>
+   <button onClick={() => covidPrediction(7)}>aaaa</button>
+   <div style={{backgroundColor: "red", width: "100vw"}} className="users">
+      {prediction.map((dia) => (
+        <div  className="user">{prediction.dia}</div>
+      ))}
+    </div>
+   </>
   )
 }
 
